e536fb59dd243c7d38dffd5383f952cf7f59ff78
Tanvir Rahman Shuvo <shuvotanvir702@gmail.com>
2025-03-26 21:18:02 +0600

    Create pagenation.jsx

---
+++ src/components/pagenation.jsx
@@ -0,0 +1,10 @@
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    return (
+      <div className="pagination">
+        <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+        <span>{currentPage} / {totalPages}</span>
+        <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>Next</button>
+      </div>
+    );
+  };
+  
